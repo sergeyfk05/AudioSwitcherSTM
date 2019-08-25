98ad53c3765ee5bc72b549f1d09c5bc1f37b7c15
Firsau Sergei <sergeyfk05@gmail.com>
2019-08-25 11:37:25 +0300

    final

--- AudioSwitcherNew/Settings.cpp
+++ AudioSwitcherNew/Settings.cpp
@@ -1,12 +1,12 @@
 
 #define DEBUG0 0
-#define DEBUG1 0
+#define DEBUG1 1
 #define DEBUG2 0
-#define MaxEqualToZeroValue 5
+#define MaxEqualToZeroValue 3
 #define MaxAvarageForNoise (float)0.4
-#define MaxAvarageForActiveNoise (float)0.03
+#define MaxAvarageForActiveNoise (float)0.06
 #define CountOfConsecutiveZeroValueForNoise 250
-#define MinCountOfZeroValue 120
+#define MinCountOfZeroValue 550
 #define USE_LED 1
 #define LED_GPIO_PERIPH RCC_APB2Periph_GPIOC
 #define LED_GPIO_GROUP GPIOC
