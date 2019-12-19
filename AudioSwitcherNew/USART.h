1d51866824a0eb325a201bbd069e43cdf49f20ec
Firsau Sergei <sergeyfk05@gmail.com>
2019-12-19 10:09:46 +0300

    fix merge problems

--- AudioSwitcherNew/USART.h
+++ AudioSwitcherNew/USART.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "Settings.cpp"
 
-#include <stm32f4xx.h>
-#include <stm32f4xx_rcc.h>
-#include <stm32f4xx_usart.h>
-#include <stm32f4xx_gpio.h>
+#include <stm32f10x.h>
+#include <stm32f10x_rcc.h>
+#include <stm32f10x_usart.h>
+#include <stm32f10x_gpio.h>
 #if DEBUG0 == 1 || DEBUG1 == 1 || DEBUG2 == 1
 
 #include <string.h>
